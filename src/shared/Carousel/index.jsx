206c5b213212f26b87d2e453f37aa1220ef55389
AJTJ <ajtj@AJTJs-MacBook-Pro.local>
2020-10-07 15:22:39 -0400

    updated carousel

--- src/shared/Carousel/index.jsx
+++ src/shared/Carousel/index.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 // import { css } from "@emotion/core";
 import Carousel from "react-elastic-carousel";
-import { MarginedContainer, SectionContainer } from "../Layout";
+import {
+  MarginedContainer,
+  SectionContainer as ImportedSectionContainer,
+} from "../Layout";
 import { ModalLayer, ModalContent } from "../Modal";
 import { useWindowDimensions } from "../../utilsJSX";
 
@@ -10,20 +13,41 @@ const CarouselContainer = styled.div`
   grid-column: span 12;
 `;
 
-const ImgWrapper = styled.div`
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
+const ImageWrapper = styled.div`
+  background-color: ${(p) => p.theme.colors.light_accent};
+  width: 100%;
+  padding: 2px;
+  margin: 5px;
 `;
 
-const Image = styled.img`
-  align-self: flex-start;
-  flex-shrink: 0;
-  min-width: 100%;
-  min-height: 100%;
-  max-height: 120%;
+const Img = styled.div`
+  height: 200px;
+  /* display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden; */
+  margin: 5px;
+  /* background-color: ${(p) => p.theme.colors.light_accent}; */
+
+  background-image: url(${(p) => p?.image});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  /* width: 100%; */
+`;
+
+// const Image = styled.img`
+//   align-self: flex-start;
+//   flex-shrink: 0;
+//   min-width: 100%;
+//   min-height: 100%;
+//   max-height: 120%;
+// `;
+
+const SectionContainer = styled(ImportedSectionContainer)`
+  /* background-color: ${(p) => p.theme.colors.light_accent}; */
+  padding: 25px 0;
+  margin: 25px 0;
 `;
 
 const CarouselComponent = (props) => {
@@ -47,16 +71,15 @@ const CarouselComponent = (props) => {
             >
               {!!images.length &&
                 images.map(({ image }, i) => (
-                  <ImgWrapper key={image + i}>
-                    <Image
-                      onClick={() => {
-                        setModalVisible(true);
-                        setCurrentImage(image);
-                      }}
-                      src={image}
-                      alt=""
-                    />
-                  </ImgWrapper>
+                  <ImageWrapper
+                    onClick={() => {
+                      setModalVisible(true);
+                      setCurrentImage(image);
+                    }}
+                    key={image + i}
+                  >
+                    <Img image={image} />
+                  </ImageWrapper>
                 ))}
             </Carousel>
           </CarouselContainer>
